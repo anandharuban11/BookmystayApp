68882662e635ded4607adb5d71ee4e20a45c57d5
anandharuban11 <anandharuban111@gmail.com>
2026-04-02 15:40:21 +0530

    Use Case 5:booking request

---
+++ BookMyStay/src/BookingRequestQueue.java
@@ -0,0 +1,20 @@
+public class BookingRequestQueue {
+
+    private Queue<Reservation> requestQueue;
+
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
+    }
+
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
+    }
+
+    public Reservation getNextRequest() {
+        return requestQueue.poll();
+    }
+
+    public boolean hasPendingRequests() {
+        return !requestQueue.isEmpty();
+    }
+}
